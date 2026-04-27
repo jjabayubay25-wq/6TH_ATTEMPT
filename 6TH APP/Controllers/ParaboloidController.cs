8523a99992a611416f47c69e2e7bef9e9838ae69
jjabayubay25-wq <jjabayubay25@bpsu.edu.ph>
2026-04-27 15:12:34 +0800

    Add project files.

---
+++ 6TH APP/Controllers/ParaboloidController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParaboloidApp.Models;
+
+namespace ParaboloidApp.Controllers
+{
+    public class ParaboloidController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(double a = 2.0, double b = 3.0, double h = 5.0)
+        {
+            var model = new ParaboloidViewModel
+            {
+                A = a,
+                B = b,
+                H = h
+            };
+            return View(model);
+        }
+    }
+}
---
+++ 6TH APP/Models/ParaboloidViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ParaboloidApp.Models
+{
+    public class ParaboloidViewModel
+    {
+        public double A { get; set; } = 2.0;
+        public double B { get; set; } = 3.0;
+        public double H { get; set; } = 5.0;
+
+        // Formula: V = 1/2 * PI * a * b * h^2
+        public double Volume => 0.5 * Math.PI * A * B * Math.Pow(H, 2);
+    }
+}
